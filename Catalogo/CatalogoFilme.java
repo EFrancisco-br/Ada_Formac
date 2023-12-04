098de6fd1a7e377fda5d271e9c481fc65cf51c48
EFrancisco-br <59630977+EFrancisco-br@users.noreply.github.com>
2023-12-04 09:50:52 -0300

    Update CatalogoFilme.java

--- Catalogo/CatalogoFilme.java
+++ Catalogo/CatalogoFilme.java
@@ -2,6 +2,8 @@ package Catalogo;
 import java.util.List;
 import java.util.ArrayList;
 
+// ATENÇÃO: por enquanto esta classa não foi utilizada
+
 public class CatalogoFilme {
     List<Filme> filmes = new ArrayList<>();
     List<Diretor> diretores = new ArrayList<>();
